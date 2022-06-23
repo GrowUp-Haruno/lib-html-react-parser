0fc698b7c67222c403c1d2c244ae5a65ee26da39
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-06-23 13:55:16 +0900

    区切り

--- src/components/PureTags.tsx
+++ src/components/PureTags.tsx
@@ -7,3 +7,6 @@ export const h4: FC<{ children: JSX.Element }> = (props) => <h4>{props.children}
 export const h5: FC<{ children: JSX.Element }> = (props) => <h5>{props.children}</h5>;
 export const p: FC<{ children: JSX.Element }> = (props) => <p>{props.children}</p>;
 export const ul: FC<{ children: JSX.Element }> = (props) => <ul>{props.children}</ul>;
+export const li: FC<{ children: JSX.Element }> = (props) => <li>{props.children}</li>;
+export const strong: FC<{ children: JSX.Element }> = (props) => <strong>{props.children}</strong>;
+export const a: FC<{ children: JSX.Element }> = (props) => <a href='./'>{props.children}</a>;
