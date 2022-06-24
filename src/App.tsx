69f929fb94ee9a4afa55611fac02334776eb4423
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-06-24 15:28:11 +0900

    区切り

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
 import './App.css';
 
-import parth, { Element } from 'html-react-parser';
+import parth, { domToReact, Element } from 'html-react-parser';
 import { jsxElementGenerator } from './lib/jsxElementGenerator';
 import React from 'react';
 
-// const sampleHtml: string =
-//   '<h2 id="hdb41525ba7">ブログテンプレートから作成されました🎉</h2><p>ブログテンプレートからAPIを作成しました。<br>おつかれさまでした🎉<br></p><h2 id="hf45076424a">APIプレビューを試そう🚀</h2><p>最初に「APIプレビュー」をしてみましょう。<br>入稿したコンテンツはAPI経由で取得し、Viewに繋ぎ込みます。<br>APIプレビューでは実際のAPIレスポンスを確認でき、あなたの開発を加速させます。<br><br>👇まずはここをクリックします。<br><img src="https://images.microcms-assets.io/assets/157404dc1ca1414195671e7b5ffd8c86/a646785aae4a41f581712bd01111bcd2/blog-template-description1.png" alt=""><br><br>APIプレビュー画面が開いたら、<strong>「取得」</strong>ボタンでリクエストを試してみましょう。<br><img src="https://images.microcms-assets.io/assets/157404dc1ca1414195671e7b5ffd8c86/4e0f1da46745436abf90df0c47ac07f0/blog-template-description2.png" alt=""><br><br>この記事の内容がAPIで取得できていることがわかります。<br><img src="https://images.microcms-assets.io/assets/157404dc1ca1414195671e7b5ffd8c86/1b6940bff275439686132bb38da936bb/blog-template-description3.png" alt=""></p><h2 id="h4c50a0f1e5"><br>次にやること🏃‍♂️</h2><p>APIプレビューで確認したレスポンスを参考に、あなた自身のWebサイトを構築しましょう。<br>microCMSはAPIでコンテンツを取得するため、お好きな言語・フレームワークで画面を構築できます。<br></p><ul><li><a href="https://document.microcms.io/tutorial/javascript/javascript-top" target="_blank" rel="noopener noreferrer">JavaScript SDK</a></li><li><a href="https://document.microcms.io/tutorial/nuxt/nuxt-top" target="_blank" rel="noopener noreferrer">Nuxt SDK</a></li><li><a href="https://document.microcms.io/tutorial/gatsby/gatsby-top" target="_blank" rel="noopener noreferrer">Gatsby SDK</a></li></ul><p><br>その他に<a href="https://microcms.io/features/sdk" target="_blank" rel="noopener noreferrer">サーバーサイドSDK（PHP / Go / Ruby）やモバイルSDK（iOS / Android）</a>もございます。<br><br>お困りなことや疑問点などございましたらお気軽にご連絡ください。<br><br>サポート窓口：<a href="mailto:support@microcms.io" target="_blank" rel="noopener noreferrer">support@microcms.io</a><br>よくある質問：<a href="https://help.microcms.io/ja/knowledge" target="_blank" rel="noopener noreferrer">https://help.microcms.io/ja/knowledge</a></p>';
+// const sampleHtml: string = `<p>a<br/>b<br/></p>`;
+// const sample = <a>test</a>
+const sampleHtml: string =
+  '<h2 id="hdb41525ba7">ブログテンプレートから作成されました🎉</h2><p>ブログテンプレートからAPIを作成しました。<br>おつかれさまでした🎉<br></p><h2 id="hf45076424a">APIプレビューを試そう🚀</h2><p>最初に「APIプレビュー」をしてみましょう。<br>入稿したコンテンツはAPI経由で取得し、Viewに繋ぎ込みます。<br>APIプレビューでは実際のAPIレスポンスを確認でき、あなたの開発を加速させます。<br><br>👇まずはここをクリックします。<br><img src="https://images.microcms-assets.io/assets/157404dc1ca1414195671e7b5ffd8c86/a646785aae4a41f581712bd01111bcd2/blog-template-description1.png" alt=""><br><br>APIプレビュー画面が開いたら、<strong>「取得」</strong>ボタンでリクエストを試してみましょう。<br><img src="https://images.microcms-assets.io/assets/157404dc1ca1414195671e7b5ffd8c86/4e0f1da46745436abf90df0c47ac07f0/blog-template-description2.png" alt=""><br><br>この記事の内容がAPIで取得できていることがわかります。<br><img src="https://images.microcms-assets.io/assets/157404dc1ca1414195671e7b5ffd8c86/1b6940bff275439686132bb38da936bb/blog-template-description3.png" alt=""></p><h2 id="h4c50a0f1e5"><br>次にやること🏃‍♂️</h2><p>APIプレビューで確認したレスポンスを参考に、あなた自身のWebサイトを構築しましょう。<br>microCMSはAPIでコンテンツを取得するため、お好きな言語・フレームワークで画面を構築できます。<br></p><ul><li><a href="https://document.microcms.io/tutorial/javascript/javascript-top" target="_blank" rel="noopener noreferrer">JavaScript SDK</a></li><li><a href="https://document.microcms.io/tutorial/nuxt/nuxt-top" target="_blank" rel="noopener noreferrer">Nuxt SDK</a></li><li><a href="https://document.microcms.io/tutorial/gatsby/gatsby-top" target="_blank" rel="noopener noreferrer">Gatsby SDK</a></li></ul><p><br>その他に<a href="https://microcms.io/features/sdk" target="_blank" rel="noopener noreferrer">サーバーサイドSDK（PHP / Go / Ruby）やモバイルSDK（iOS / Android）</a>もございます。<br><br>お困りなことや疑問点などございましたらお気軽にご連絡ください。<br><br>サポート窓口：<a href="mailto:support@microcms.io" target="_blank" rel="noopener noreferrer">support@microcms.io</a><br>よくある質問：<a href="https://help.microcms.io/ja/knowledge" target="_blank" rel="noopener noreferrer">https://help.microcms.io/ja/knowledge</a></p>';
 
-const sampleHtml: string = '<h2>テスト</h2><h2>テスト</h2>  ';
+// const sampleHtml: string = '<h2>テスト</h2><h2>テスト</h2>  ';
 function App() {
-  let index: number = 0;
   const a = parth(sampleHtml, {
+    // replace: (node) => {
+    //   if (!(node instanceof Element)) return null;
+    //   if (!(node.parent === null)) return null;
+    //   const resultElements: Array<JSX.Element> = [];
+    //   const list = node.children.map((child) => {
+    //     if (child instanceof Element) {
+    //       return { child: child, name: child.name, data: '' };
+    //     } else if (child instanceof Text) {
+    //       return { child: child, name: '', data: child.data };
+    //     }
+    //     return { child: child, name: '', data: '' };
+    //   });
+    //   while (list.length) {
+    //     let listCount = 1;
+    //     if (list[0].name !== 'br') {
+    //       // 次のbrタグまでの配列の長さを測定
+    //       while (list[0].name !== 'img' && list.length > listCount && list[listCount].name !== 'br') {
+    //         listCount++;
+    //       }
+    //       // 0番地からbrタグ手前までを抜き出す
+    //       const results = list.splice(0, listCount);
+    //       // 内容をセットする
+    //       if (node.name === 'p') {
+    //         resultElements.push(<p>{domToReact([...results.map((result) => result.child)])}</p>);
+    //       }
+    //     } else {
+    //       // 主にbrなど不要なタグを削除
+    //       list.splice(0, 1);
+    //     }
+    //   }
+    //   const temp = resultElements.map((result,index) => <React.Fragment key={index}>{result}</React.Fragment>)
+    //   return temp;
+    // },
+
     replace: (domNode) => {
       if (!(domNode instanceof Element)) return null;
       if (!(domNode.parent === null)) return null;
@@ -18,12 +53,13 @@ function App() {
       const resultElements: Array<JSX.Element> = [];
 
       resultElements.push(...jsxElementGenerator(domNode));
-      index++;
-      return <React.Fragment key={index}>{resultElements.map((result) => result)}</React.Fragment>;
+        const temp = resultElements.map((result,index) => <React.Fragment key={index}>{result}</React.Fragment>)
+        return temp;
+      // return <React.Fragment >{resultElements.map((result) => result)}</React.Fragment>;
     },
   });
 
-  return <React.Fragment>{a}</React.Fragment>;
+  return <>{a}</>;
 }
 
 export default App;
--- src/lib/jsxElementGenerator.tsx
+++ src/lib/jsxElementGenerator.tsx
@@ -1,46 +1,45 @@
 import { Element } from 'html-react-parser';
 import { Text, Node } from 'domhandler';
-import { FC } from 'react';
+import { FC, Fragment } from 'react';
 
-import { h2 } from '../components/PureTags';
-// import { a, h1, h2, h3, h4, h5, img, li, p, strong, ul } from '../components/PureTags';
+import { a, h1, h2, h3, h4, h5, img, li, p, strong, ul } from '../components/PureTags';
 
 export const jsxElementGenerator = (domNode: Element) => {
   const resultElements: Array<JSX.Element> = [];
   switch (domNode.name) {
-    // case 'h1':
-    //   innerJsxElementGenerator(domNode, resultElements, h1);
-    //   break;
+    case 'h1':
+      innerJsxElementGenerator(domNode, resultElements, h1);
+      break;
     case 'h2':
       innerJsxElementGenerator(domNode, resultElements, h2);
       break;
-    // case 'h3':
-    //   innerJsxElementGenerator(domNode, resultElements, h3);
-    //   break;
-    // case 'h4':
-    //   innerJsxElementGenerator(domNode, resultElements, h4);
-    //   break;
-    // case 'h5':
-    //   innerJsxElementGenerator(domNode, resultElements, h5);
-    //   break;
-    // case 'p':
-    //   innerJsxElementGenerator(domNode, resultElements, p);
-    //   break;
-    // case 'ul':
-    //   innerJsxElementGenerator(domNode, resultElements, ul);
-    //   break;
-    // case 'li':
-    //   innerJsxElementGenerator(domNode, resultElements, li);
-    //   break;
-    // case 'strong':
-    //   innerJsxElementGenerator(domNode, resultElements, strong);
-    //   break;
-    // case 'a':
-    //   innerJsxElementGenerator(domNode, resultElements, a);
-    //   break;
-    // case 'img':
-    //   innerJsxElementGenerator(domNode, resultElements, img);
-    //   break;
+    case 'h3':
+      innerJsxElementGenerator(domNode, resultElements, h3);
+      break;
+    case 'h4':
+      innerJsxElementGenerator(domNode, resultElements, h4);
+      break;
+    case 'h5':
+      innerJsxElementGenerator(domNode, resultElements, h5);
+      break;
+    case 'p':
+      innerJsxElementGenerator(domNode, resultElements, p);
+      break;
+    case 'ul':
+      innerJsxElementGenerator(domNode, resultElements, ul);
+      break;
+    case 'li':
+      innerJsxElementGenerator(domNode, resultElements, li);
+      break;
+    case 'strong':
+      innerJsxElementGenerator(domNode, resultElements, strong);
+      break;
+    case 'a':
+      innerJsxElementGenerator(domNode, resultElements, a);
+      break;
+    case 'img':
+      innerJsxElementGenerator(domNode, resultElements, img);
+      break;
     default:
       console.log(`${domNode.name}が未定義です`);
       break;
@@ -53,7 +52,8 @@ const innerJsxElementGenerator = (
   resultElements: Array<JSX.Element>,
   Component: FC<{
     children?: JSX.Element;
-    key: number;
+    key: string;
+    id: string;
     anchorAttr?: React.AnchorHTMLAttributes<HTMLAnchorElement>;
     imgAttr?: React.ImgHTMLAttributes<HTMLImageElement>;
   }>
@@ -67,9 +67,17 @@ const innerJsxElementGenerator = (
     return { child: child, name: '', data: '' };
   });
 
-  // if (domNode.name === 'img') {
-  //   resultElements.push(<Component imgAttr={domNode.attribs} />);
-  // }
+  let loopCount = 1;
+
+  if (domNode.name === 'img') {
+    resultElements.push(
+      <Component
+        imgAttr={domNode.attribs}
+        key={`${resultElements.length}-${loopCount}`}
+        id={`${resultElements.length}-${loopCount}`}
+      />
+    );
+  }
 
   while (list.length) {
     let listCount = 1;
@@ -81,20 +89,26 @@ const innerJsxElementGenerator = (
       }
       // 0番地からbrタグ手前までを抜き出す
       const results = list.splice(0, listCount);
+
       // 内容をセットする
       resultElements.push(
-        <Component key={resultElements.length}>
+        <Component
+          key={`${resultElements.length}-${loopCount}-${results[0].name}`}
+          id={`${resultElements.length}-${loopCount}-${results[0].name}`}
+        >
           <>
             {results.map((result) => {
               return resultsMap(result);
             })}
           </>
         </Component>
+
       );
     } else {
       // 主にbrなど不要なタグを削除
       list.splice(0, 1);
     }
+    loopCount++;
   }
 };
 
@@ -102,7 +116,8 @@ const resultsMap = (result: { child: Node; name: string; data: string }) => {
   if (result.child instanceof Element) {
     const innerResultElements: Array<JSX.Element> = [];
     innerResultElements.push(...jsxElementGenerator(result.child));
-    return <>{innerResultElements.map((result) => result)}</>;
+    const temp = innerResultElements.map((result,index) => <Fragment key={index}>{result}</Fragment>);
+    return temp;
   } else if (result.child instanceof Text) {
     return result.child.data;
   } else {
